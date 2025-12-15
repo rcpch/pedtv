12d2058eb38eb699491b8b56d06682b3fc7f423d
Michael Barton <michael.barton@rcpch.ac.uk>
2025-12-15 12:46:43 +0000

    Fix logo

--- cms/settings.py
+++ cms/settings.py
@@ -117,9 +117,10 @@ SITE_ID = 1
 # PORTAL_LOGO_DARK_SVG = ""
 # PORTAL_LOGO_LIGHT_SVG = ""
 # place the files on static/images folder
-PORTAL_LOGO_DARK_SVG = "/static/images/logo_dark.svg"
+# [mbarton pedtv] Don't have SVG versions of the logo unfortunately
+# PORTAL_LOGO_DARK_SVG = "/static/images/logo_dark.svg"
 PORTAL_LOGO_DARK_PNG = "/static/images/logo_dark.png"
-PORTAL_LOGO_LIGHT_SVG = "/static/images/logo_light.svg"
+# PORTAL_LOGO_LIGHT_SVG = "/static/images/logo_light.svg"
 PORTAL_LOGO_LIGHT_PNG = "/static/images/logo_dark.png"
 
 # paths to extra css files to be included, eg "/static/css/custom.css"
